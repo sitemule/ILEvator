7c162382832b2b907736afa00645ab56c6231fad
Mihael Schmidt <mihael@rpgnextgen.com>
2023-02-23 16:47:32 +0000

    Added C interface for request module

---
+++ headers/request.h
@@ -0,0 +1,11 @@
+#ifndef ILEVATOR_REQUEST_H
+#define ILEVATOR_REQUEST_H
+
+#include "ostypes.h"
+#include "varchar.h"
+
+PVOID iv_request_new(VARCHAR12 method, VARCHAR url, VARCHAR1024 mimeType);
+void iv_request_dispose(PVOID request);
+LVARPUCHAR iv_request_toString(PVOID request);
+
+#endif
