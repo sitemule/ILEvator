11bdb28fe38685783836b668549d281fdff3a05c
Mihael Schmidt <mihael@rpgnextgen.com>
2023-09-17 10:22:18 +0000

    Fixed multiple content-type header when using multipart

--- integrationtests/helidon/src/main/java/ilevator/MultiPartResource.java
+++ integrationtests/helidon/src/main/java/ilevator/MultiPartResource.java
@@ -1,5 +1,7 @@
 package ilevator;
 
+import java.util.logging.Logger;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -12,11 +14,15 @@ import org.glassfish.jersey.media.multipart.FormDataParam;
 @Path("multipart")
 public class MultiPartResource {
 
+    private final Logger logger = Logger.getLogger(MultiPartResource.class.getName());
+
 	@POST
 	@Path("form")
 	@Consumes(MediaType.MULTIPART_FORM_DATA)
 	public Response postFormDataTextOnly(@FormDataParam("user") FormDataBodyPart username,
 			@FormDataParam("email") FormDataBodyPart email) {
+		logger.info(username.getValue() + " - " + email.getValue());
+		
 		return Response.ok(username.getValue() + " - " + email.getValue()).build();
 	}
 
